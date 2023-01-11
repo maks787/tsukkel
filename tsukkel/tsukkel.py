b6980d963bef1b2c7bd522ee4841aa63f8b4378b
opilane <opilane@HP-CZC2349HT1>
2023-01-11 11:52:08 +0200

    Add project files.

---
+++ tsukkel/tsukkel.py
@@ -0,0 +1,16 @@
+﻿import random
+n=random.randint(1,10)
+while True:
+    text = input("Введите число или стоп для выхода: ")
+    if text == "стоп":
+        print("Выход из программы! До встречи! Загадано было", n)
+        break # инструкция выхода из цикла
+    elif int(text) == n:
+        print("Победа")
+        break
+    else:
+        print("Попробуйте еще")
+
+
+
+     
